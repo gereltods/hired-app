e4a8f4a99ae0c90559b67fc9e3bf625a35258944
Gerelt-Od <oju-mac@oju-mac.local>
2022-08-16 11:29:41 +0800

    fix cors

--- src/app/application.go
+++ src/app/application.go
@@ -13,7 +13,7 @@ var route = router.New()
 func StartApplication() {
 	withCors := cors.NewCorsHandler(cors.Options{
 		// if you leave allowedOrigins empty then fasthttpcors will treat it as "*"
-		AllowedOrigins: []string{}, // Only allow example.com to access the resource
+		//AllowedOrigins: string, // Only allow example.com to access the resource
 		// if you leave allowedHeaders empty then fasthttpcors will accept any non-simple headers
 		AllowedHeaders: []string{"Authorization", "Accept", "Origin", "DNT", "X-CustomHeader", "Keep-Alive", "User-Agent", "X-Requested-With", "If-Modified-Since", "Cache-Control", "Content-Type", "Content-Range", "Range", "x-api-role", "X-Total-Count"}, // only allow x-something-client and Content-Type in actual request
 		// if you leave this empty, only simple method will be accepted
